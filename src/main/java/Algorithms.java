1f941e26ddf97afc635df9499ecdb1853da5ed27
ins <insiderz1@bk.ru>
2021-02-05 03:20:53 +0300

    update calcCharValues: now we get char index from ASCII table

--- src/main/java/Algorithms.java
+++ src/main/java/Algorithms.java
@@ -65,42 +65,12 @@ public class Algorithms {
      * @param value int
      */
     public static int calcCharValues(String value) {
-        Map<String, Integer> alphabet = new HashMap<>() {{
-            put("A", 1);
-            put("B", 2);
-            put("C", 3);
-            put("D", 4);
-            put("E", 5);
-            put("F", 6);
-            put("G", 7);
-            put("H", 8);
-            put("I", 9);
-            put("J", 10);
-            put("K", 11);
-            put("L", 12);
-            put("M", 13);
-            put("N", 14);
-            put("O", 15);
-            put("P", 16);
-            put("Q", 17);
-            put("R", 18);
-            put("S", 19);
-            put("T", 20);
-            put("U", 21);
-            put("V", 22);
-            put("W", 23);
-            put("X", 24);
-            put("Y", 25);
-            put("Z", 26);
-        }};
-
-        // map value as array
-        String[] valueArr = value.split("");
-
         int result = 0;
-
+        int offset = 64;
+        char[] valueArr = value.toUpperCase().toCharArray();
         for (int i = 0; i < value.length(); i++) {
-            result += Math.pow(alphabet.size(), valueArr.length - i - 1) * alphabet.get(valueArr[i]);
+            int index = valueArr[i] - offset;
+            result += Math.pow(26, valueArr.length - i - 1) * index;
         }
         return result;
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
+
     }
 
 
