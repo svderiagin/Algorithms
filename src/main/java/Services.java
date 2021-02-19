d11bf542319c13e1c7f4fd44365f8b7ad86f8196
ins <insiderz1@bk.ru>
2021-02-19 23:46:26 +0300

    add service for getting datetime plus days

--- src/main/java/Services.java
+++ src/main/java/Services.java
@@ -4,21 +4,21 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.TimeZone;
 
 public class Services {
 
     /**
      * https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
      */
-    public static String getCurrentDateTime() {
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        return dateFormat.format(date);
+    public static String getDateTimeCurrent(String format) {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static String getDateTimePlusDays(String format, int days) {
+        return LocalDateTime.now().plusDays(days).format(DateTimeFormatter.ofPattern(format));
     }
 
     /**
